ed7e17de0ad44873c3e9abf2633c3eba49c6b475
gopika-gopal <140744560+gopika-gopal@users.noreply.github.com>
2024-12-12 22:55:23 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import rewriteAll from 'vite-plugin-rewrite-all';
 import tailwindcss from 'tailwindcss';
 
+export default defineConfig({
+  plugins: [rewriteAll()],
+  base: '/GymFluencer/',
 
-// export default defineConfig({
-//   plugins: [rewriteAll()],
-//   base: '/GymFluencer', 
-
-  export default defineConfig({
-    css: {
-      postcss: {
-        plugins: [tailwindcss()],
-      },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
     },
+  },
 });
+
